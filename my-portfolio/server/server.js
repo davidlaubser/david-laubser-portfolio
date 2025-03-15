7699b4193be20c9eb4e6d2f88985b677a8c4a41e
davidlaubser <davidlaubser12246@gmail.com>
2025-03-15 17:39:53 +0200

    Updated server port configuration

--- my-portfolio/server/server.js
+++ my-portfolio/server/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5007;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
@@ -52,6 +52,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
